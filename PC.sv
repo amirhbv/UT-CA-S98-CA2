35cc38205c34084027e75085ef561b298085202f
AmirHossein Habibvand <ahhabibvand@gmail.com>
2019-04-12 11:06:32 +0430

    Add PC

---
+++ PC.sv
@@ -0,0 +1,13 @@
+module PC(
+	input clk ,
+	input rst ,
+	input[9:0] load_in ,
+	output reg[9:0] pc_cur
+);
+	always @(negedge clk , posedge rst) begin
+		if ( rst )
+			pc_cur <= 16'd0 ;
+		else
+			pc_cur <= load_in ;
+	end
+endmodule
